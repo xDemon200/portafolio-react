cb522daabfd5531ecb9196885092d0be6602aa50
Juan carlos munoz guzman <jcmunoz@acsyt.com>
2020-07-20 20:17:07 -0500

    Updates

---
+++ precache-manifest.127da5531c4ca6d86490780123e3fc44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b1845663e1e6a70a424ea82ecbc3d5",
+    "url": "/portafolio-react/index.html"
+  },
+  {
+    "revision": "7fab1f7e068f83902e1c",
+    "url": "/portafolio-react/static/css/main.8b5d00fa.chunk.css"
+  },
+  {
+    "revision": "261d2bc9ac42868f887b",
+    "url": "/portafolio-react/static/js/2.69665a73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portafolio-react/static/js/2.69665a73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fab1f7e068f83902e1c",
+    "url": "/portafolio-react/static/js/main.d8ac14aa.chunk.js"
+  },
+  {
+    "revision": "e27d990c5748b0bf827f",
+    "url": "/portafolio-react/static/js/runtime-main.f2844bf1.js"
+  }
+]);
